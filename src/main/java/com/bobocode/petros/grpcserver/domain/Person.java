d2894ec40427be59a9148cc70da9a49c807d9019
Andrii.Kovalchuk <shtramak@gmail.com>
2022-10-20 21:30:56 +0300

    grpc server implementation

---
+++ src/main/java/com/bobocode/petros/grpcserver/domain/Person.java
@@ -0,0 +1,20 @@
+package com.bobocode.petros.grpcserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private LocalDate dateOfBirth;
+    private String email;
+}
